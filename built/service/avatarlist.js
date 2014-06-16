b5b85262ad1b7875e9c4833393451afdb5806eea
Vincent Weyl <vincent.weyl@taiaut.com>
2014-06-16 16:39:17 -0700

    built browser version - copied to js-min directory for deployment

--- built/service/avatarlist.js
+++ built/service/avatarlist.js
@@ -1,8 +1,8 @@
-/**
+/*
  * https://github.com/IDEAFY/Ideafy
  * Proprietary License - All rights reserved
  * Author: Vincent Weyl <vincent@ideafy.com>
  * Copyright (c) 2014 IDEAFY LLC
  */
 
-define(["OObject","Bind.plugin","Event.plugin","service/config","service/utils","Store"],function(e,t,i,n,s,a){function o(e){var s,o=new a([]),r=n.get("avatars");for(this.plugins.addAll({avatar:new t(o,{setAvatar:function(e){this.setAttribute("style","background-image: url('"+e+"');")}}),event:new i(this)}),this.template='<ul data-avatar="foreach"><li data-avatar="bind: setAvatar, img; bind: name, id"></li></ul>',s=0;s<e.length;s++)e[s]===n.get("user").get("_id")?o.alter("push",{id:e[s],img:n.get("avatar")}):r.get(e[s])?o.alter("push",{id:e[s],img:r.get(e[s])}):n.get("transport").request("GetAvatar",{id:e[s]},function(t){t.error||o.alter("push",{id:e[s],img:t})})}return function(t){return o.prototype=new e,new o(t)}});
+define(["OObject","Bind.plugin","Store","service/avatar"],function(e,t,i,n){function s(e){var s=new i([]);e.forEach(function(e){s.alter("push",{id:e})}),this.plugins.addAll({avatar:new t(s,{setAvatar:function(e){if(e){var t=document.createDocumentFragment(),i=new n([e]);i.place(t),this.hasChildNodes()?this.replaceChild(t,this.firstChild):this.appendChild(t)}}})}),this.template='<ul data-avatar="foreach"><li data-avatar="bind: setAvatar, id"></li></ul>'}return function(t){return s.prototype=new e,new s(t)}});
